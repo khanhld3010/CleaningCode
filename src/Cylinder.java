67b67ed3438ca0024e7d76a301054351401f0da8
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-09 13:51:39 +0700

    Cylinder

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,16 @@
 public class Cylinder {
     public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
-        double volume = perimeter * height + 2 * baseArea;
-        return volume;
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        return  perimeter * height + 2 * baseArea;
+
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
